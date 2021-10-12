097204e93a9f35ee85549e5d5e78eb413f2af24d
Gen2102 <genesis@s2dioapps.com>
2021-10-12 12:01:31 +0800

    CreateTask

--- app/src/main/java/com/s2dioapps/metask/ui/component/createtask/view/CreateTaskActivity.kt
+++ app/src/main/java/com/s2dioapps/metask/ui/component/createtask/view/CreateTaskActivity.kt
@@ -2,11 +2,19 @@ package com.s2dioapps.metask.ui.component.createtask.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.s2dioapps.metask.R
 
 class CreateTaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_task)
+
+        findViewById<Button>(R.id.ct_back)?.setOnClickListener {
+            onBackPressed()
+        }
+        findViewById<Button>(R.id.btn_cancel)?.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/s2dioapps/metask/ui/component/tasktoday/view/TaskTodayFragment.kt
+++ app/src/main/java/com/s2dioapps/metask/ui/component/tasktoday/view/TaskTodayFragment.kt
@@ -1,12 +1,16 @@
 package com.s2dioapps.metask.ui.component.tasktoday.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.s2dioapps.metask.R
 import com.s2dioapps.metask.databinding.FragmentTaskTodayBinding
+import com.s2dioapps.metask.ui.component.createtask.view.CreateTaskActivity
 import com.s2dioapps.metask.ui.component.tasktoday.viewmodel.TaskTodayViewModel
 
 class TaskTodayFragment : Fragment() {
@@ -24,19 +28,23 @@ class TaskTodayFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         taskTodayViewModel =
             ViewModelProvider(this).get(TaskTodayViewModel::class.java)
 
         _binding = FragmentTaskTodayBinding.inflate(inflater, container, false)
+
         val root: View = binding.root
 
 
         return root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
+        binding.floatingButtonTaskToday.setOnClickListener {
+            startActivity(Intent(requireContext(), CreateTaskActivity::class.java))
+        }
     }
 
     override fun onDestroyView() {
